omar-os/omar

Follow the text: the diagram, and Tab
Three things reported against the editor and the drawing.

The diagram did not change when the program did, so an editor showed a
topology that no longer existed. The client has no compiler to build one
with, so the projection now carries the compiled snapshot — the same
request that already runs on every pause in typing. Only while nothing is
running: a live run's diagram is a record of what is happening, not of
what has been typed since.

Tab left the editor. A textarea gives it to the browser, which moves focus
mid-word; in a code pane it indents.

Triggers and effects are a reaction's own wiring, not a path a value
travels between ports, and were drawn the same as the edges that are.
They are dashed now, and solid is reserved for connections.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

web/app/studio.tsx
@@ -461,6 +461,10 @@ export function Studio({
           setSourceErrors(result.ok ? [] : (result.errors ?? []));
           setSteps(result.steps ?? []);
           setTruncated(result.truncated ?? false);
+          // The drawing follows the text. Only while nothing is running: a live
+          // run's diagram is a record of what is happening, not of what has
+          // been typed since.
+          if (result.preview && !run) setSnapshot(result.preview);
           // A recomputed projection replaces the tail, so a hand-held position
           // past its end would be pointing at nothing.
           setStepIndex((current) => Math.min(current, Math.max(0, (result.steps?.length ?? 1) - 1)));
@@ -477,7 +481,9 @@ export function Studio({
       clearTimeout(timer);
       abort.abort();
     };
-  }, [source, filename, serveUrl, canRun, present]);
+    // `run` decides whether the drawing may be replaced, so a run appearing
+    // has to re-run this rather than leave a stale rule in a closure.
+  }, [source, filename, serveUrl, canRun, present, run]);
 
   const isDeployed = run !== null;
 

lsk567/oma

web/app/studio.tsx
@@ -501,6 +501,10 @@ export function Studio({
           setSourceErrors(result.ok ? [] : (result.errors ?? []));
           setSteps(result.steps ?? []);
           setTruncated(result.truncated ?? false);
+          // The drawing follows the text. Only while nothing is running: a live
+          // run's diagram is a record of what is happening, not of what has
+          // been typed since.
+          if (result.preview && !run) setSnapshot(result.preview);
           // A recomputed projection replaces the tail, so a hand-held position
           // past its end would be pointing at nothing.
           setStepIndex((current) => Math.min(current, Math.max(0, (result.steps?.length ?? 1) - 1)));
@@ -517,7 +521,11 @@ export function Studio({
       clearTimeout(timer);
       abort.abort();
     };
-  }, [source, filename, serveUrl, canRun, presentKey]);
+    // `run` decides whether the drawing may be replaced, so a run appearing
+    // has to re-run this rather than leave a stale rule in a closure. The key
+    // rather than the array: this effect sets the snapshot `present` is
+    // derived from, so depending on its identity would ask forever.
+  }, [source, filename, serveUrl, canRun, presentKey, run]);
 
   const isDeployed = run !== null;
 